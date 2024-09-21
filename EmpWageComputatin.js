7f2a575d902be85a5447c3170fe0e629fa0766c7
SanthoshSekar21 <santhosh20sekar@gmail.com>
2024-09-21 10:17:54 +0530

    [santhoosh] added: the DailyWageCalculation feature in EmpWageComputation

--- EmpWageComputatin.js
+++ EmpWageComputatin.js
@@ -1,13 +1,37 @@
-function empWageComputationMain(){
-    console.log('Welcome to the Employee WageComputation');
-    checkAttendance();
-};
+async function empWageComputationMain() {
+    console.log('Welcome to the Employee Wage Computation');
+    try {
+        let attendance = await checkAttendance();
+        const calculateWage=await dailyWageCalculation(attendance);
+        console.log( `employee is present \n wage of the employee is Rs. ${calculateWage}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 const checkAttendance = () => {
-    const attendance = Math.floor(Math.random() * 2);
-    if (attendance === 1) 
-        console.log('Present');
-     else 
-        console.log('Absent');
+    return new Promise((resolve, reject) => {
+        const attendance = Math.floor(Math.random() * 2);
+        if (attendance === 1) 
+            resolve('Present');
+        else 
+            reject('Absent');
+    });
 };
+const fullDayHour = 8;
+const wagePerHour = 20;
+const dailyWageCalculation=(attendance)=>{
+    return new Promise((resolve,reject)=>{
+        if(attendance==='Present'){
+           const dailyWage=fullDayHour*wagePerHour;
+        resolve(dailyWage)
+        }
+        else{
+            reject(' Employee is Absent no wage')
+        }
+    })
+}
+// Execute the main function
 empWageComputationMain();
+
+
